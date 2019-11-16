2cfff1477b1aeca16aafaf08e21c100053c177c9
MrTolerant <lebedevpetr@outlook.com>
2019-11-17 02:18:11 +0300

    nocache

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -5,19 +5,20 @@ const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const GitRevisionPlugin = require('git-revision-webpack-plugin');
+
 const gitRevisionPlugin = new GitRevisionPlugin();
-const StringReplacePlugin = require("string-replace-webpack-plugin");
+const StringReplacePlugin = require('string-replace-webpack-plugin');
 const uuidv4 = require('uuid/v4')
 
 const config = {
   entry: [
-    './main.js',    
+    './main.js',
     './assets/scss/main.scss'
   ],
   resolve: {
-      alias: {
-        'd3': 'd3/index.js'
-      }
+    alias: {
+      'd3': 'd3/index.js'
+    }
   },
   output: {
     filename: 'js/bundle.js',
@@ -28,43 +29,43 @@ const config = {
   context: resolve(__dirname, 'client'),
   devtool: false,
   performance: {
-      hints: false,
-      maxEntrypointSize: 512000,
-      maxAssetSize: 512000
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000
   },
-  module: {      
+  module: {
     rules: [
-      { 
+      {
         test: /.html$/,
         loader: StringReplacePlugin.replace({
-            replacements: [
-                {
-                    pattern: /COMMITHASH/ig,
-                    replacement: function (match, p1, offset, string) {
-                        return gitRevisionPlugin.commithash();
-                    }
-                }
-            ]
-        })
-    },      
-    { 
-      test: /\.js$/,
-      loader: StringReplacePlugin.replace({
           replacements: [
             {
               pattern: /COMMITHASH/ig,
-              replacement: function (match, p1, offset, string) {
+              replacement (match, p1, offset, string) {
+                        return gitRevisionPlugin.commithash();
+                    }
+            }
+          ]
+        })
+      },
+      {
+        test: /\.js$/,
+        loader: StringReplacePlugin.replace({
+          replacements: [
+            {
+              pattern: /COMMITHASH/ig,
+              replacement (match, p1, offset, string) {
                   return gitRevisionPlugin.commithash();
               }
             }
           ]
-      })
-  },      
+        })
+      },
       {
-        enforce: "pre",
+        enforce: 'pre',
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: "eslint-loader"
+        loader: 'eslint-loader'
       },
       {
         test: /\.js$/,
@@ -72,12 +73,12 @@ const config = {
           'babel-loader',
         ],
         exclude: /node_modules/,
-      },      
+      },
       {
         test: /\.css$/,
         use: ExtractTextPlugin.extract({
           fallback: 'style-loader',
-          use: [ 'css-loader' ],
+          use: ['css-loader'],
           publicPath: '../_build'
         }),
       },
@@ -191,14 +192,17 @@ const config = {
         }
       },
     }),
-    new webpack.optimize.ModuleConcatenationPlugin(),    
+    new webpack.optimize.ModuleConcatenationPlugin(),
     new ExtractTextPlugin({ filename: 'css/main.css', disable: false, allChunks: true }),
     new CopyWebpackPlugin([{ from: 'assets/images', to: 'images' }]),
     new CopyWebpackPlugin([{ from: 'assets/fonts', to: 'fonts' }]),
 
     new CopyWebpackPlugin([{ from: 'vendors', to: 'vendors' }]),
     new CopyWebpackPlugin([{ from: 'assets/manifest.json', to: 'manifest.json' }]),
-    new CopyWebpackPlugin([{ from: 'html.js', to: 'html.js', transform: (content) => {
+    new CopyWebpackPlugin([{
+ from: 'html.js',
+to: 'html.js',
+transform: (content) => {
       return content.toString().replace(/COMMITHASH/g, uuidv4());
     }
     }]),
