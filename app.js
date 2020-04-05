1075b1ac16b917037efc881fc7ee3cf666f6c223
18274774 <pvllebedev@sberbank.ru>
2020-04-05 18:35:09 +0300

    mongo connect ok

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
-const express = require("express");
-const config = require("config");
-const mongoose = require("mongoose");
-const app = express();
+const express = require('express')
+const config = require('config')
+const mongoose = require('mongoose')
+const app = express()
 
-const PORT = config.get("port") || 5000;
+const PORT = config.get('port') || 5000
 
-async function start() {}
-try {
-  await mongoose.connect(config.get(mongoUri));
-} catch (error) {
-  console.log("server error", error.message);
-  process.exit(1);
+async function start() {
+  try {
+    await mongoose.connect(config.get('mongoUri'), {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true
+    })
+    app.listen(PORT, () => console.log(`app hs been started at port ${PORT}`))
+  } catch (error) {
+    console.log('server error', error.message)
+    process.exit(1)
+  }
 }
 
-start();
-app.listen(PORT, () => console.log(`app hs been started at port ${PORT}`));
+start()
