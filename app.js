b54ec0014164d81d9df98df1a71d61f7c970d6d6
18274774 <pvllebedev@sberbank.ru>
2020-04-05 17:55:19 +0300

    app.js

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log("app");
+const express = require("express");
+const config = require("config");
+const mongoose = require("mongoose");
+const app = express();
+
+const PORT = config.get("port") || 5000;
+
+async function start() {}
+try {
+  await mongoose.connect(config.get(mongoUri));
+} catch (error) {
+  console.log("server error", error.message);
+  process.exit(1);
+}
+
+start();
+app.listen(PORT, () => console.log(`app hs been started at port ${PORT}`));
