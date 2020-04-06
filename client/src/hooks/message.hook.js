4259435c834026080763670c9a024191301044b8
Petr <petr@Petrs-Mac-mini.local>
2020-04-06 12:19:55 +0300

    toast ok

--- client/src/hooks/message.hook.js
+++ client/src/hooks/message.hook.js
@@ -1,9 +1,13 @@
 import { useCallback } from 'react'
+import { toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+
+toast.configure()
 
 export const useMessage = () => {
   return useCallback((text) => {
     if (text) {
-      alert(text)
+      toast(text)
     }
   }, [])
 }
