7e040b3a4bc5e765ac45a45e63b1357e8d91fc29
18274774 <pvllebedev@sberbank.ru>
2020-04-06 15:11:02 +0300

    navbar

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router } from 'react-router-dom'
 import { useRoutes } from './routes'
 import { useAuth } from './hooks/auth.hook'
 import { AuthContext } from './context/AuthContext'
+import { NavBar } from './components/NavBar'
 
 function App() {
   const { token, login, logout, userId } = useAuth()
@@ -19,6 +20,7 @@ function App() {
       }}
     >
       <Router>
+        {isAuthenticated && <NavBar />}
         <div className="min-w-screen min-h-screen">{routes}</div>
       </Router>
     </AuthContext.Provider>
---
+++ client/src/components/NavBar.js
@@ -0,0 +1,43 @@
+import React, { useContext } from 'react'
+import { NavLink, useHistory } from 'react-router-dom'
+import { AuthContext } from '../context/AuthContext'
+
+export const NavBar = () => {
+  const auth = useContext(AuthContext)
+  const history = useHistory()
+  const logoutHandler = (event) => {
+    event.preventDefault()
+    auth.logout()
+    history.push('/')
+  }
+
+  return (
+    <div className="bg-white shadow-xl rounded-full w-screen flex flex-row items-center p-1  bg-white shadow-xs">
+      <div className="ml-8 text-lg text-gray-700 hidden md:flex">
+        <a href="/">URL shortener</a>
+      </div>
+      <div className="flex flex-row-reverse mr-4 ml-4 md:hidden">
+        <i className="fas fa-bars"></i>
+      </div>
+      <div className="flex flex-row mr-8 hidden md:flex justify-start">
+        <NavLink to="/create">
+          <div className="shadow-xl rounded-full text-gray-700 text-center bg-gray-300 px-4 py-2 m-2">
+            Create
+          </div>
+        </NavLink>
+        <NavLink to="/links">
+          <div className="shadow-xl rounded-full text-gray-700 text-center bg-gray-300 px-4 py-2 m-2">
+            Links
+          </div>
+        </NavLink>
+      </div>
+      <div className="ml-auto flex flex-row mr-8 hidden md:flex justify-end">
+        <button type="button" onClick={logoutHandler}>
+          <div className="shadow-xl rounded-full text-gray-700 text-center bg-gray-300 px-4 py-2 m-2">
+            Logout
+          </div>
+        </button>
+      </div>
+    </div>
+  )
+}
