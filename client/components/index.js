142b9cc74f36d0945876b110121e9a5c02f060b5
Petr <petr@Petrs-Mac-mini.local>
2020-04-08 00:05:01 +0300

    url short

--- client/components/index.js
+++ client/components/index.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React from 'react'
 
-import { connect } from 'react-redux';
+import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import Head from './head'
 
@@ -15,25 +15,70 @@ const Index = () => {
           <header id="header">
             {firefly}
             <h1>Petr Lebedev</h1>
-            <p> DevOps &nbsp;&bull;&nbsp; Fullstack Developer &nbsp;&bull;&nbsp; Cyborg &nbsp;&bull;&nbsp; Never sleep</p>
+            <p>
+              {' '}
+              DevOps &nbsp;&bull;&nbsp; Fullstack Developer &nbsp;&bull;&nbsp;
+              Cyborg &nbsp;&bull;&nbsp; Never sleep
+            </p>
             <nav>
               <ul>
-                <li><a href="https://twitter.com/MrTolerant" rel="noopener noreferrer" target="_blank" className="icon brands fa-twitter"><span className="label">Twitter</span></a></li>
-                <li><a href="https://www.linkedin.com/in/lebedevpetr/" rel="noopener noreferrer" target="_blank" className="icon brands fa-linkedin"><span className="label">Linkedin</span></a></li>
+                <li>
+                  <a href="https://url.mrtolerant.ru" className="fas fa-link">
+                    <span className="label">URL Shortener</span>
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://twitter.com/MrTolerant"
+                    rel="noopener noreferrer"
+                    target="_blank"
+                    className="icon brands fa-twitter"
+                  >
+                    <span className="label">Twitter</span>
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://www.linkedin.com/in/lebedevpetr/"
+                    rel="noopener noreferrer"
+                    target="_blank"
+                    className="icon brands fa-linkedin"
+                  >
+                    <span className="label">Linkedin</span>
+                  </a>
+                </li>
                 {/* <li><a href="#" className="icon brands fa-dribbble"><span className="label">Dribbble</span></a></li> */}
-                <li><a href="https://github.com/MrTolerant" rel="noopener noreferrer" target="_blank" className="icon brands fa-github"><span className="label">Github</span></a></li>
-                <li><a href="mailto:lebedevpetr@outlook.com" rel="noopener noreferrer" target="_blank" className="icon solid fa-envelope"><span className="label">Email</span></a></li>
+                <li>
+                  <a
+                    href="https://github.com/MrTolerant"
+                    rel="noopener noreferrer"
+                    target="_blank"
+                    className="icon brands fa-github"
+                  >
+                    <span className="label">Github</span>
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="mailto:lebedevpetr@outlook.com"
+                    rel="noopener noreferrer"
+                    target="_blank"
+                    className="icon solid fa-envelope"
+                  >
+                    <span className="label">Email</span>
+                  </a>
+                </li>
               </ul>
             </nav>
           </header>
 
           <footer id="footer">
-            <span className="copyright">&copy; Design: <a href="http://MrTolerant.ru">LPV</a>.</span>
+            <span className="copyright">
+              &copy; Design: <a href="http://MrTolerant.ru">LPV</a>.
+            </span>
           </footer>
-
         </div>
       </div>
-
     </div>
   )
 }
