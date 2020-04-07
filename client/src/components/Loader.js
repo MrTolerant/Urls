b48c72b4aaf7ecd03d7ac187e76254a4da9ab530
18274774 <pvllebedev@sberbank.ru>
2020-04-07 20:32:36 +0300

    spinner

--- client/src/components/Loader.js
+++ client/src/components/Loader.js
@@ -2,8 +2,6 @@ import React from 'react'
 
 export const Loader = () => (
   <div className="bg-gray-200 flex flex-wrap min-h-screen min-w-screen">
-    <div className="m-auto loader ease-linear rounded border-8 border-t-8 border-gray-200 h-64 w-64">
-      Loading...
-    </div>
+    <span className="mx-auto spinner text-4xl" />
   </div>
 )
